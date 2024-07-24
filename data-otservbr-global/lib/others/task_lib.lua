2ab11f8325965a74ac0945c9f40b65f65c81b3e7
Tentacionxa <fencore05@gmail.com>
2024-07-24 01:40:32 +0100

    change

--- data-otservbr-global/lib/others/task_lib.lua
+++ data-otservbr-global/lib/others/task_lib.lua
@@ -6,6 +6,513 @@ rewards = {
 	},
 },
 
+{name = "Minotaur", color = 40, total = 5000, type = "once", storage = 190000, storagecount = 190001, 
+rewards = {
+{5804, 1},
+{"exp", 1000000},
+	},
+},
+
+{name = "Dragon", color = 40, total = 1000, type = "daily", storage = 190002, storagecount = 190003, 
+rewards = {
+{3043, 20},
+{5908, 1},
+{"exp", 1500000},
+	},
+},
+
+{name = "Dragon Lord", color = 40, total = 1000, type = "once", storage = 190004, storagecount = 190005, 
+rewards = {
+{5919, 1},
+{"exp", 1500000},
+	},
+},
+
+{name = "Amazon", color = 40, total = 500, type = "repeatable", storage = 190008, storagecount = 190009, 
+rewards = { 
+{"exp", 1500000},
+	},
+},
+
+{name = "Valkyrie", color = 40, total = 2000, type = "once", storage = 190010, storagecount = 190011, 
+rewards = { 
+{3437, 1},
+	},
+},
+
+{name = "Lizard Legionnaire", color = 40, total = 2000, type = "repeatable", storage = 190531, storagecount = 190532, 
+rewards = { 
+{3043, 10},
+	},
+},
+{name = "Lizard Dragon Priest", color = 40, total = 2000, type = "repeatable", storage = 190533, storagecount = 190534, 
+rewards = { 
+{3043, 10},
+	},
+},
+{name = "Lizard High Guard", color = 40, total = 2000, type = "repeatable", storage = 190535, storagecount = 190536, 
+rewards = { 
+{3043, 15},
+	},
+},
+{name = "Lizard Zaogun", color = 40, total = 2000, type = "repeatable", storage = 190537, storagecount = 190538, 
+rewards = { 
+{3043, 30},
+	},
+},
+{name = "Draken Warmaster", color = 40, total = 2000, type = "repeatable", storage = 190539, storagecount = 190540, 
+rewards = { 
+{3043, 40},
+	},
+},
+{name = "Draken Spellweaver", color = 40, total = 2000, type = "repeatable", storage = 190541, storagecount = 190542, 
+rewards = { 
+{3043, 40},
+	},
+},
+{name = "Lizard Chosen", color = 40, total = 2000, type = "repeatable", storage = 190543, storagecount = 190544, 
+rewards = { 
+{3043, 50},
+{22516, 5},
+	},
+},
+{name = "Ghastly Dragon", color = 40, total = 2000, type = "repeatable", storage = 190545, storagecount = 190546, 
+rewards = { 
+{3043, 70},
+{22516, 5},
+	},
+},
+{name = "Draken Elite", color = 40, total = 2000, type = "repeatable", storage = 190547, storagecount = 190548, 
+rewards = { 
+{3043, 50},
+{22516, 5},
+	},
+},
+{name = "Draken Abomination", color = 40, total = 2000, type = "repeatable", storage = 190549, storagecount = 190560, 
+rewards = { 
+{3043, 30},
+{22516, 5},
+	},
+},
+{name = "Adult Goanna", color = 40, total = 1000, type = "repeatable", storage = 190561, storagecount = 190562, 
+rewards = { 
+{22516, 10},
+{3043, 50},
+	},
+},
+{name = "Young Goanna", color = 40, total = 1000, type = "repeatable", storage = 190563, storagecount = 190564, 
+rewards = { 
+{22516, 10},
+{3043, 30},
+	},
+},
+{name = "Betrayed Wraith", color = 40, total = 1000, type = "repeatable", storage = 190565, storagecount = 190566, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Nightmare", color = 40, total = 1000, type = "repeatable", storage = 190567, storagecount = 190568, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Lost Soul", color = 40, total = 1000, type = "repeatable", storage = 190569, storagecount = 190570, 
+rewards = { 
+{22516, 1},
+	},
+},
+{name = "Plaguesmith", color = 40, total = 1000, type = "repeatable", storage = 190571, storagecount = 190572, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Dark Torturer", color = 40, total = 1000, type = "repeatable", storage = 190573, storagecount = 190574, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Demon Outcast", color = 40, total = 1000, type = "repeatable", storage = 190575, storagecount = 190576, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Blightwalker", color = 40, total = 1000, type = "repeatable", storage = 190577, storagecount = 190578, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Hellhound", color = 40, total = 1000, type = "repeatable", storage = 190579, storagecount = 190580, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Frazzlemaw", color = 40, total = 1000, type = "repeatable", storage = 190581, storagecount = 190582, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Silencer", color = 40, total = 1000, type = "repeatable", storage = 190583, storagecount = 190584, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Retching Horror", color = 40, total = 1000, type = "repeatable", storage = 190585, storagecount = 190586, 
+rewards = { 
+{22516, 3},
+	},
+},
+
+{name = "Choking Fear", color = 40, total = 1000, type = "repeatable", storage = 190587, storagecount = 190588, 
+rewards = { 
+{22516, 3},
+	},
+},
+
+{name = "Guzzlemaw", color = 40, total = 1000, type = "repeatable", storage = 190589, storagecount = 190590, 
+rewards = { 
+{22516, 5},
+	},
+},
+{name = "Sight of Surrender", color = 40, total = 1000, type = "repeatable", storage = 190591, storagecount = 190592, 
+rewards = { 
+{22721, 25},
+	},
+},
+
+{name = "Weakened Frazzlemaw", color = 40, total = 1000, type = "repeatable", storage = 190593, storagecount = 190594, 
+rewards = { 
+{22516, 1},
+	},
+},
+
+{name = "Sphinx", color = 40, total = 1000, type = "repeatable", storage = 190595, storagecount = 190596, 
+rewards = { 
+{22516, 5},
+{3043, 40},
+	},
+},
+{name = "Lamassu", color = 40, total = 1000, type = "repeatable", storage = 190791, storagecount = 190792, 
+rewards = { 
+{22516, 5},
+{3043, 60},
+	},
+},
+{name = "Crypt Warden", color = 40, total = 1000, type = "repeatable", storage = 190597, storagecount = 190598, 
+rewards = { 
+{22516, 4},
+{3043, 50},
+	},
+},
+{name = "Priestess of the Wild Sun", color = 40, total = 1000, type = "repeatable", storage = 190599, storagecount = 190600, 
+rewards = { 
+{22721, 2},
+	},
+},
+{name = "Black Sphinx Acolyte", color = 40, total = 1000, type = "repeatable", storage = 190601, storagecount = 190602, 
+rewards = { 
+{22721, 3},
+	},
+},
+{name = "Burning Gladiator", color = 40, total = 1000, type = "repeatable", storage = 190603, storagecount = 190604, 
+rewards = { 
+{22721, 5},
+	},
+},
+{name = "Deepling Guard", color = 40, total = 1000, type = "repeatable", storage = 190605, storagecount = 190606, 
+rewards = { 
+{14142, 1},
+	},
+},
+
+{name = "Deepling Warrior", color = 40, total = 1000, type = "repeatable", storage = 190607, storagecount = 190608, 
+rewards = { 
+{"exp", 1000000},
+	},
+},
+
+{name = "Deepling Scout", color = 40, total = 1000, type = "repeatable", storage = 190609, storagecount = 190610, 
+rewards = { 
+{"exp", 1000000},
+	},
+},
+
+{name = "Guzzlemaw", color = 40, total = 1000, type = "once", storage = 190611, storagecount = 190612, 
+rewards = { 
+{20270, 1},
+	},
+},
+
+{name = "Frazzlemaw", color = 40, total = 1000, type = "once", storage = 190613, storagecount = 190614, 
+rewards = { 
+{20272, 1},
+	},
+},
+
+{name = "Silencer", color = 40, total = 1000, type = "once", storage = 190615, storagecount = 190616, 
+rewards = { 
+{20271, 1},
+	},
+},
+
+{name = "Medusa", color = 40, total = 1000, type = "once", storage = 190617, storagecount = 190618, 
+rewards = { 
+{3393, 1},
+	},
+},
+
+{name = "Demon", color = 40, total = 1000, type = "once", storage = 190028, storagecount = 190029, 
+rewards = { 
+{3365, 1},
+	},
+},
+
+{name = "Hero", color = 40, total = 5000, type = "once", storage = 190030, storagecount = 190031, 
+rewards = { 
+{3394, 1},
+	},
+},
+{name = "Glooth Brigand", color = 40, total = 1000, type = "once", storage = 190133, storagecount = 190134, 
+rewards = { 
+{3043, 20},
+	},
+},
+{name = "Glooth Bandit", color = 40, total = 1000, type = "once", storage = 190131, storagecount = 190132, 
+rewards = { 
+{3043, 20},
+	},
+},
+{name = "Ogre Sage", color = 40, total = 1000, type = "repeatable", storage = 190570, storagecount = 190571, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Ogre Ruffian", color = 40, total = 1000, type = "repeatable", storage = 190572, storagecount = 190573, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Ogre Rowdy", color = 40, total = 1000, type = "repeatable", storage = 190575, storagecount = 190576, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Ogre Shaman", color = 40, total = 1000, type = "repeatable", storage = 190589, storagecount = 190590, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Gazer Spectre", color = 40, total = 1000, type = "repeatable", storage = 190596, storagecount = 190597, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Thanatursus", color = 40, total = 1000, type = "repeatable", storage = 190598, storagecount = 190599, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Priestess Of The Wild Sun", color = 40, total = 1000, type = "repeatable", storage = 190580, storagecount = 190581, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Black Sphinx Acolyte", color = 40, total = 1000, type = "repeatable", storage = 190582, storagecount = 190583, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Burning Gladiator", color = 40, total = 1000, type = "repeatable", storage = 190585, storagecount = 190586, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Pirat Bombardier", color = 40, total = 1000, type = "repeatable", storage = 190532, storagecount = 190533, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Pirat Mate", color = 40, total = 1000, type = "repeatable", storage = 190534, storagecount = 190535, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Pirat Cutthroat", color = 40, total = 1000, type = "repeatable", storage = 190536, storagecount = 190537, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Pirat Scoundrel", color = 40, total = 1000, type = "repeatable", storage = 190538, storagecount = 190539, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Cloak Of Terror", color = 40, total = 1000, type = "repeatable", storage = 190032, storagecount = 190033, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Hellhound", color = 40, total = 500, type = "repeatable", storage = 190732, storagecount = 190733, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Nightmare", color = 40, total = 500, type = "repeatable", storage = 190734, storagecount = 190735, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Demon", color = 40, total = 500, type = "repeatable", storage = 190736, storagecount = 190737, 
+rewards = { 
+{"exp", 300000},
+	},
+},
+{name = "Vibrant Phantom", color = 40, total = 1000, type = "repeatable", storage = 190034, storagecount = 190035, 
+rewards = { 
+{"exp", 3000000},
+	},
+},
+
+{name = "Courage Leech", color = 40, total = 1000, type = "repeatable", storage = 190036, storagecount = 190037, 
+rewards = { 
+{"exp", 3000000},
+	},
+},
+
+{name = "Brachiodemon", color = 40, total = 1000, type = "repeatable", storage = 190038, storagecount = 190039, 
+rewards = { 
+{"exp", 3000000},
+	},
+},
+
+{name = "Infernal Demon", color = 40, total = 1000, type = "once", storage = 190040, storagecount = 190041, 
+rewards = { 
+{34109, 2},
+	},
+},
+
+{name = "Infernal Phantom", color = 40, total = 1000, type = "repeatable", storage = 190042, storagecount = 190043, 
+rewards = { 
+{"exp", 3000000},
+	},
+},
+{name = "Demon Outcast", color = 40, total = 1000, type = "repeatable", storage = 190680, storagecount = 190681, 
+rewards = { 
+{"exp", 3000000},
+	},
+},
+{name = "Juggernaut", color = 40, total = 1000, type = "once", storage = 190044, storagecount = 190045, 
+rewards = { 
+{3422, 1},
+	},
+},
+
+{name = "Dawnfire Asura", color = 40, total = 1000, type = "repeatable", storage = 190046, storagecount = 190047, 
+rewards = { 
+	{3043, 40},
+{"exp", 2000000},
+	},
+},
+{name = "Midnight Asura", color = 40, total = 1000, type = "repeatable", storage = 197432, storagecount = 197433, 
+rewards = { 
+	{3043, 40},
+{"exp", 2000000},
+	},
+},
+{name = "Frost Flower Asura", color = 40, total = 1000, type = "repeatable", storage = 190070, storagecount = 190071, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Freakish Lost Soul", color = 40, total = 1000, type = "repeatable", storage = 190073, storagecount = 190074, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Flimsy Lost Soul", color = 40, total = 1000, type = "repeatable", storage = 190075, storagecount = 190076, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Mean Lost Soul", color = 40, total = 1000, type = "repeatable", storage = 190077, storagecount = 190078, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Crazed Winter Vanguard", color = 40, total = 1000, type = "repeatable", storage = 190080, storagecount = 190081, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Crazed Winter Rearguard", color = 40, total = 1000, type = "repeatable", storage = 190082, storagecount = 190083, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Soul-Broken Harbinger", color = 40, total = 1000, type = "repeatable", storage = 190084, storagecount = 190085, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Breach Brood", color = 40, total = 1000, type = "repeatable", storage = 190086, storagecount = 190087, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Reality Reaver", color = 40, total = 1000, type = "repeatable", storage = 190088, storagecount = 190089, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Sparkion", color = 40, total = 1000, type = "repeatable", storage = 190091, storagecount = 190092, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Werelion", color = 40, total = 1000, type = "repeatable", storage = 190094, storagecount = 190095, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Werelioness", color = 40, total = 1000, type = "repeatable", storage = 190096, storagecount = 190097, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Dark Carnisylvan", color = 40, total = 1000, type = "repeatable", storage = 190646, storagecount = 190647, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Poisonous Carnisylvan", color = 40, total = 1000, type = "repeatable", storage = 190648, storagecount = 190649, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Hulking Carnisylvan", color = 40, total = 1000, type = "repeatable", storage = 190650, storagecount = 190651, 
+rewards = { 
+{"exp", 2000000},
+	},
+},
+{name = "Girtablilu Warrior", color = 40, total = 1000, type = "once", storage = 190052, storagecount = 190053, 
+rewards = {   
+{"exp", 10000000},
+	},
+},
+
+{name = "Dark Carnisylvan", color = 40, total = 1000, type = "once", storage = 190062, storagecount = 190063, 
+rewards = { 
+{"exp", 10000000},
+	},
+},
 }
 
 squareWaitTime = 5000

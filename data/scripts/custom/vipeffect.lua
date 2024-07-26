5db96ae5ddb657c4cace521e06b9a49e6ff3abf5
Tentacionxa <fencore05@gmail.com>
2024-07-26 05:02:02 +0100

    change

---
+++ data/scripts/custom/vipeffect.lua
@@ -0,0 +1,21 @@
+local globalevent = GlobalEvent("staffvip")
+
+local staffEffect = 40
+local vipEffect = 50
+
+function globalevent.onThink(interval)
+    for _, player in ipairs(Game.getPlayers()) do
+        local position = player:getPosition()
+        if player:getGroup():getAccess() and not player:isInGhostMode() then
+            player:say("STAFF", TALKTYPE_MONSTER_SAY)
+            position:sendMagicEffect(staffEffect)
+        elseif player:isVip() then
+            player:say("VIP", TALKTYPE_MONSTER_SAY)
+            position:sendMagicEffect(vipEffect)
+        end
+    end
+    return true
+end
+
+globalevent:interval(5000)
+globalevent:register()

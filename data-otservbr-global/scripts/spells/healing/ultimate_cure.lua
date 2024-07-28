6979584ec93d3463040903a980bd95a5455ee501
Tentacionxa <fencore05@gmail.com>
2024-07-28 01:36:44 +0100

    change

--- data-otservbr-global/scripts/spells/healing/ultimate_cure.lua
+++ data-otservbr-global/scripts/spells/healing/ultimate_cure.lua
@@ -1,19 +1,26 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_FIRE, CONDITION_ENERGY, CONDITION_CURSED, CONDITION_POISON)
-combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
-
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
-	return combat:execute(creature, variant)
+    if creature:getCondition(CONDITION_FIRE) then
+        creature:removeCondition(CONDITION_FIRE)
+    end
+    if creature:getCondition(CONDITION_CURSED) then
+        creature:removeCondition(CONDITION_CURSED)
+    end
+    if creature:getCondition(CONDITION_ENERGY) then
+        creature:removeCondition(CONDITION_ENERGY)
+    end
+    if creature:getCondition(CONDITION_POISON) then
+        creature:removeCondition(CONDITION_POISON)
+    end
+    return true
 end
 
 spell:name("Ultimate Cure")
 spell:words("exana ultimate")
 spell:group("healing")
 spell:vocation("sorcerer;true", "master sorcerer;true","druid;true", "elder druid;true", "paladin;true", "royal paladin;true", "knight;true", "elite knight;true")
-spell:castSound(SOUND_EFFECT_TYPE_SPELL_CURE_BURNING, SOUND_EFFECT_TYPE_SPELL_CURE_POISON, SOUND_EFFECT_TYPE_SPELL_CURE_ELECTRIFICATION, SOUND_EFFECT_TYPE_SPELL_CURE_CURSE)
+spell:castSound(SOUND_EFFECT_TYPE_SPELL_CURE_BURNING)
 spell:id(600)
 spell:cooldown(6000)
 spell:groupCooldown(1000)

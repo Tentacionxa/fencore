0c3a6db7d5038a26be464c641cadf2ba592d7734
Tentacionxa <fencore05@gmail.com>
2024-07-29 03:25:40 +0100

    change

--- data-otservbr-global/scripts/actions/other/cooldownbosspotions.lua
+++ data-otservbr-global/scripts/actions/other/cooldownbosspotions.lua
@@ -3,12 +3,16 @@ bosspotion = 19731
 local bosspotion = Action()
 
 function bosspotion.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+    local bosspotion = bosspotion
+	if not bosspotion then
+		return false
+	end
+else
 	Player:setBossCooldown("Faceless Bane", 0)
     Player:setBossCooldown("Tentugly's Head", 0)
-
-
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You removed cooldown on all bosses")
-	return true
+player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You removed cooldown on all bosses")
+end
+return true
 end
 
 bosspotion:id(bosspotion)

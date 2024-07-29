0d5d2c96a1f229e0c069d0c8e2c790518b556a2a
Tentacionxa <fencore05@gmail.com>
2024-07-29 03:34:49 +0100

    change

--- data-otservbr-global/scripts/actions/other/cooldownbosspotions.lua
+++ data-otservbr-global/scripts/actions/other/cooldownbosspotions.lua
@@ -1,8 +1,12 @@
 local bosspotion = Action()
 function bosspotion.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if not player:canFightBoss("Faceless Bane") then
 		Player:setBossCooldown("Faceless Bane", 0)
     Player:setBossCooldown("Tentugly's Head", 0)
 		player:sendTextMessage(19, "You can fight the bosses again!")
+	else
+		player:sendTextMessage(19, "You access to the bosses has already been released!")
+	end
 
 	return true
 end

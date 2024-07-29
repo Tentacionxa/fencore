bf0ed6a9f1eabc2ee3403972948a000efd229912
Tentacionxa <fencore05@gmail.com>
2024-07-29 20:41:04 +0100

    change

--- data-otservbr-global/npc/onkan.lua
+++ data-otservbr-global/npc/onkan.lua
@@ -119,6 +119,7 @@ npcConfig.shop = {
 	{ itemName = "shockwave amulet", clientId = 9304, buy = 4000 },
 	{ itemName = "might ring", clientId = 3048, buy = 5000 },
 	{ itemName = "orc's boss reminder", clientId = 19155, buy = 500000 },
+	{ itemName = "enchanted theurgic amulet", clientId = 30403, buy = 2000000 },
 }
 -- On buy npc shop message
 npcType.onBuyItem = function(npc, player, itemId, subType, amount, ignore, inBackpacks, totalCost)

1799f433c09b34d6c91f2265d77d8cc85816614a
Tentacionxa <fencore05@gmail.com>
2024-07-29 04:32:47 +0100

    change

--- data-otservbr-global/scripts/actions/other/cooldownbosspotions.lua
+++ data-otservbr-global/scripts/actions/other/cooldownbosspotions.lua
@@ -1,14 +1,15 @@
+local bosses = {'Faceless Bane', 'Tentugly\'s Head', 'The Duke Of The Depths', 'The Baron From Below', 'Urmahlullu The Weakened', 'Ravenous Hunger', 'Unaz The Mean', 'Vok The Freakish', 'Black Vixen', 'Sharpclaw', 'Darkfang', 'Bloodback', 'Irgix The Flimsy', 'The Fear Feaster', 'The Count Of The Core', 'Razzagorn', 'Mazoran', 'The Unwelcome', 'Drume', 'Magma Bubble', 'Goshnar\'s Cruelty', 'Timira The Many-Headed', 'Shulgrax', 'Scarlett Etzel', 'Grand Master Oberon', 'Ratmiral Blackwhiskers', 'Duke Krule', 'Alptramun', 'Maxxenius', 'Izcandar The Banished', 'Anomaly', 'World Devourer', 'Outburst', 'Eradicator', 'Rupture', 'King Zelos', 'Ferumbras Mortal Shell', 'Goshnar\'s Megalomania', 'Goshnar\'s Malice', 'Goshnar\'s Hatred', 'Lloyd', 'Lady Tenebris', 'The Enraged Thorn Knight', 'Dragonking Zyrtarch'}
+
 local bosspotion = Action()
 function bosspotion.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if not player:canFightBoss("Faceless Bane") then
-		player:setBossCooldown("Faceless Bane", 0)
-    player:setBossCooldown("Tentugly's Head", 0)
-		player:sendTextMessage(19, "You can fight the bosses again!")
-        player:removeItem(19371, 1)
+	for _, name in bosses do
+		player:setBossCooldown(name, 0)
+		player:sendTextMessage(19, "You can fight all the bosses again!")
+        
 	else
-		player:sendTextMessage(19, "You access to the bosses has already been released!")
+		player:sendTextMessage(19, "You can still fight bosses. Fight them and then use the potion!")
 	end
-
+    item:remove()
 	return true
 end
 

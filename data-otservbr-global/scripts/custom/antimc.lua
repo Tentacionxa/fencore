85a28536b046edfe9353dd3266bf51ff5ba1fe48
Tentacionxa <fencore05@gmail.com>
2024-07-30 07:51:57 +0100

    change

--- data-otservbr-global/scripts/custom/antimc.lua
+++ data-otservbr-global/scripts/custom/antimc.lua
@@ -1,9 +1,9 @@
 local config = {
-    max = 2,
-    maxVip = 2,
-    text = "Only 1 (FREE) & 1 (VIP) characters allowed per IP.",
+    max = 3,
+    maxVip = 3,
+    text = "Only 3 (FREE) & 3 (VIP) characters allowed per IP.",
     group_id = 1,  -- it apply the limit to players with a group less than or equal to the id
-    milliseconds_before_kick_to_read_popup = 1 -- 9000 = 9 seconds
+    milliseconds_before_kick_to_read_popup = 6500 -- 9000 = 9 seconds
 }
 local accepted_ip_list = {}  -- here put the Ip you whant to be allowed to use Mc/Magebomb..
 function delayedKickPlayerMCLimit(cid)

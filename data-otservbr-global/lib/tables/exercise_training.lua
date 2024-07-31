4f07b3184c463008a3b05da75ed6a20c836e23dd
Tentacionxa <fencore05@gmail.com>
2024-07-31 13:03:46 +0100

    change

--- data-otservbr-global/lib/tables/exercise_training.lua
+++ data-otservbr-global/lib/tables/exercise_training.lua
@@ -1,8 +1,10 @@
 if weapon:getAttribute(ITEM_ATTRIBUTE_CHARGES) <= 0 then
     weapon:remove(1)
+    player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You will automatically use Your next training weapon now")
     local weapon = player:getItemById(weaponId, true)
     if not weapon or (not weapon:isItem() or not weapon:hasAttribute(ITEM_ATTRIBUTE_CHARGES)) then
         player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your training weapon has disappeared.")
+        player:setTraining(true)
         LeaveTraining(playerId)
         return false
     end

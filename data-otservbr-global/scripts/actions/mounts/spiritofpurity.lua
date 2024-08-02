cf31eb63c98585ba937265deb35fe3500ee8da56
Tentacionxa <fencore05@gmail.com>
2024-08-02 14:56:24 +0100

    change

--- data-otservbr-global/scripts/actions/mounts/spiritofpurity.lua
+++ data-otservbr-global/scripts/actions/mounts/spiritofpurity.lua
@@ -3,7 +3,7 @@ SpiritualId = 44048
 local Spiritual = Action()
 
 function Spiritual.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	player:addMount(146)
+	player:addMount(217)
 	item:remove(1)
 	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You received a Spirit of Purity mount.")
 	return true

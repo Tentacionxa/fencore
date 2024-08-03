f0de059ae229781a0169bf654acf8d4ae01ad828
Tentacionxa <fencore05@gmail.com>
2024-08-03 05:07:39 +0100

    change

--- data/scripts/globalevents/teleport_texts.lua
+++ data/scripts/globalevents/teleport_texts.lua
@@ -43,6 +43,8 @@ local effects = {
 {position = Position(32557, 32400, 5), text = 'Bursted Spectre Cathedral', effect = CONST_ME_GROUNDSHAKER},
 {position = Position(32571, 32400, 5), text = 'Soul War', effect = CONST_ME_GROUNDSHAKER},
 {position = Position(32560, 32400, 5), text = 'Asura Palace', effect = CONST_ME_GROUNDSHAKER},
+{position = Position(32567, 32407, 5), text = 'Secret Library', effect = CONST_ME_GROUNDSHAKER},
+
 
 
 	{position = Position(32580, 32391, 5), text = 'Thais', effect = CONST_ME_GROUNDSHAKER},  

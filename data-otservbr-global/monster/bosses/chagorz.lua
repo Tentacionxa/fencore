c284e86bf3a37ffea9280c5a98119810fd61b32b
Tentacionxa <fencore05@gmail.com>
2024-08-03 10:01:56 +0100

    change

--- data-otservbr-global/monster/bosses/chagorz.lua
+++ data-otservbr-global/monster/bosses/chagorz.lua
@@ -27,7 +27,7 @@ monster.changeTarget = {
 
 monster.bosstiary = {
 	bossRaceId = 2366,
-	bossRace = RARITY_NEMESIS,
+	bossRace = RARITY_ARCHFOE,
         storageCooldown = Storage.Quest.U12_60.APiratesTail.ChagorzTimer
 }
 

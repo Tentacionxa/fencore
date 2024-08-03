a659c5f3397ee389fe5b6e15bcad54c619807740
Tentacionxa <fencore05@gmail.com>
2024-08-03 03:58:20 +0100

    change

--- data-otservbr-global/npc/iskan.lua
+++ data-otservbr-global/npc/iskan.lua
@@ -26,6 +26,9 @@ npcConfig.flags = {
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+
 npcType.onThink = function(npc, interval)
 	npcHandler:onThink(npc, interval)
 end
@@ -50,90 +53,19 @@ npcType.onCloseChannel = function(npc, creature)
 	npcHandler:onCloseChannel(npc, creature)
 end
 
-local function greetCallback(npc, player)
-	if player:getStorageValue(Storage.BarbarianTest.Questline) < 8 then
-		npcHandler:setMessage(MESSAGE_GREET, "You are not one of us. I don't want to talk to you.")
-	elseif player:getStorageValue(Storage.TheIceIslands.HuskyKill) >= 1 then
-		npcHandler:setMessage(MESSAGE_GREET, "I was told you have been killing huskies here in Svargrond! Never do that again, okay? They are precious animals for us. Give me 500 gold and I will forget that incident without telling it to Jarl Sven. Okay?")
-	elseif player:hasAchievement("Warlord of Svargrond") then
-		npcHandler:setMessage(MESSAGE_GREET, "HAIL to the Warlord of the arena! |PLAYERNAME|, Hero of Svargrond! What can I {do for you}?")
-	elseif player:getStorageValue(Storage.BarbarianTest.Questline) == 8 then
-		npcHandler:setMessage(MESSAGE_GREET, "Greetings. What can I {do for you}?")
+-- Travel
+local function addTravelKeyword(keyword, cost, destination, action, condition)
+	if condition then
+		keywordHandler:addKeyword({ keyword }, StdModule.say, { npcHandler = npcHandler, text = "I'm sorry but I don't travel there." }, condition)
 	end
-	return true
-end
-local function creatureSayCallback(npc, creature, type, message)
-	local player = Player(creature)
-	local playerId = player:getId()
 
-	if not npcHandler:checkInteraction(npc, creature) then
-		return false
-	end
-	if player:getStorageValue(Storage.BarbarianTest.Questline) < 8 then
-		return true
-	end
-	if MsgContains(message, "passage") then
-		if player:getStorageValue(Storage.TheIceIslands.Questline) >= 3 then
-			npcHandler:say("So, do you want a passage to Nibelor my friend?", npc, creature)
-		else
-			npcHandler:say("Do you want to Nibelor?", npc, creature)
-		end
-		npcHandler:setTopic(playerId, 2)
-	elseif MsgContains(message, "mission") then
-		if player:getStorageValue(Storage.BarbarianTest.Questline) >= 8 then -- if Barbarian Test absolved
-			if player:getStorageValue(Storage.TheIceIslands.Questline) == 1 then
-				npcHandler:say({
-					"Well, one of the boys has run away. I think he got the scent of some beast. He's young and inexperienced so I can't blame the cub ...",
-					"I would like you to see after him. He should be somewhere north west of the town. He is probably marking his territory so you should be able to find his trace. Are you willing to do that?",
-				}, npc, creature)
-				npcHandler:setTopic(playerId, 1)
-			elseif player:getStorageValue(Storage.TheIceIslands.Questline) == 2 then
-				npcHandler:say("You are a friend of mine and the boys now. I tell you something. If you ever need to go to the isle of Nibelor, just ask me for a {passage}.", npc, creature)
-				player:setStorageValue(Storage.TheIceIslands.Questline, 3)
-				player:setStorageValue(Storage.TheIceIslands.Mission01, 3) -- Questlog The Ice Islands Quest, Befriending the Musher
-				npcHandler:setTopic(playerId, 0)
-			else
-				npcHandler:say("I have now no mission for you.", npc, creature)
-				npcHandler:setTopic(playerId, 0)
-			end
-		else
-			npcHandler:say("Sorry but I only give missions to those who are considered a true Barbarian. ", npc, creature)
-		end
-	elseif MsgContains(message, "yes") then
-		if player:getStorageValue(Storage.TheIceIslands.HuskyKill) >= 1 then
-			if player:removeMoneyBank(500) then
-				npcHandler:say("Alright, we are even!", npc, creature)
-				player:setStorageValue(Storage.TheIceIslands.HuskyKillStatus, 0)
-				player:setStorageValue(Storage.TheIceIslands.HuskyKill, 0)
-			else
-				npcHandler:say("You don't have enough. Bring me the money and I will forget about it.", npc, creature)
-			end
-		elseif npcHandler:getTopic(playerId) == 1 then
-			npcHandler:say({
-				"That's surprising. Take a piece of meat. If you find the boy, feed it to him. That will give him enough strength and incentive to return to his pack ...",
-				"Talk to him by calling his name 'Sniffler' and tell him you got meat for him. After he has eaten the meat, return here to talk to me about your mission.",
-			}, npc, creature)
-			player:setStorageValue(Storage.TheIceIslands.Questline, 1)
-			player:setStorageValue(Storage.TheIceIslands.Mission01, 1) -- Questlog The Ice Islands Quest, Befriending the Musher
-			npcHandler:setTopic(playerId, 0)
-		elseif npcHandler:getTopic(playerId) == 2 then
-			if player:getStorageValue(Storage.TheIceIslands.Questline) >= 3 then
-				player:teleportTo(Position(32325, 31049, 7))
-				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-				npcHandler:setTopic(playerId, 0)
-			else
-				npcHandler:say("Sorry, first time you have to do a mission for me.", npc, creature)
-				npcHandler:setTopic(playerId, 0)
-			end
-		end
-	elseif MsgContains(message, "no") then
-		if player:getStorageValue(Storage.TheIceIslands.HuskyKill) >= 1 then
-			npcHandler:say("Mhmm, you leave me no other choice than to inform the Jarl.", npc, creature)
-			player:setStorageValue(Storage.TheIceIslands.HuskyKillStatus, 1)
-		end
-	end
-	return true
+	local travelKeyword = keywordHandler:addKeyword({ keyword }, StdModule.say, { npcHandler = npcHandler, text = "Do you seek a passage to " .. keyword:titleCase() .. " for |TRAVELCOST|?", cost = cost, discount = "postman" })
+	travelKeyword:addChildKeyword({ "yes" }, StdModule.travel, { npcHandler = npcHandler, premium = false, cost = cost, discount = "postman", destination = destination }, nil, action)
+	travelKeyword:addChildKeyword({ "no" }, StdModule.say, { npcHandler = npcHandler, text = "We would like to serve you some time.", reset = true })
 end
+
+
+addTravelKeyword("nibelor", 200, Position(32329, 31044, 7))
 --Basic
 keywordHandler:addKeyword({ "do for you" }, StdModule.say, { npcHandler = npcHandler, text = "I run the dog sled service from this city to {Nibelor}." })
 keywordHandler:addAliasKeyword({ "job" })

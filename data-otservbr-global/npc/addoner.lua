1173db0877039699908f4d775dcbc3c1ebef7e6a
Tentacionxa <fencore05@gmail.com>
2024-08-03 09:43:54 +0100

    change

--- data-otservbr-global/npc/addoner.lua
+++ data-otservbr-global/npc/addoner.lua
@@ -89,8 +89,8 @@ addoninfo = {
 ['second shaman addon'] = {cost = 0, items = {{5014,1}, {3002,5}}, outfit_female = 158, outfit_male = 154, addon = 2, storageID = 51037},
 ['first norseman addon'] = {cost = 0, items = {{7290,5}}, outfit_female = 252, outfit_male = 251, addon = 1, storageID = 51038},
 ['second norseman addon'] = {cost = 0, items = {{7290,10}}, outfit_female = 252, outfit_male = 251, addon = 2, storageID = 51039},
-['first nightmare addon'] = {cost = 0, items = {{6499,500}, {768,1}}, outfit_female = 269, outfit_male = 268, addon = 1, storageID = Storage.OutfitQuest.DefaultStart},
-['second nightmare addon'] = {cost = 0, items = {{6499,1000}, {770,1}}, outfit_female = 269, outfit_male = 268, addon = 2, storageID = Storage.OutfitQuest.DefaultStart}
+['first nightmare addon'] = {cost = 0, items = {{6499,500}, {768,1}}, outfit_female = 269, outfit_male = 268, addon = 1, storageID = Storage.OutfitQuest.BrotherhoodOutfit},
+['second nightmare addon'] = {cost = 0, items = {{6499,1000}, {770,1}}, outfit_female = 269, outfit_male = 268, addon = 2, storageID = Storage.OutfitQuest.BrotherhoodOutfit}
 }
 local o = {'citizen', 'hunter', 'knight', 'mage', 'nobleman', 'summoner', 'warrior', 'barbarian', 'druid', 'wizard', 'oriental', 'pirate', 'assassin', 'beggar', 'shaman', 'norseman', 'nightmare'}
 local rtnt = {}

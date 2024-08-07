5cf900537eecefb5b5d6b057011f7d925d1861e8
Tentacionxa <fencore05@gmail.com>
2024-08-07 21:12:35 +0100

    change

--- data/events/scripts/party.lua
+++ data/events/scripts/party.lua
@@ -66,25 +66,8 @@ function Party:onDisband()
 end
 
 function Party:onShareExperience(exp)
-	local sharedExperienceMultiplier = 2.50 --20%
-	local vocationsIds = {}
+	local totalMembers = #self:getMembers()
+	local bonusExperiencePerPlayer = totalMembers > 1 and totalMembers * 100 or 0 --percent
 
-	local vocationId = self:getLeader():getVocation():getBase():getId()
-	if vocationId ~= VOCATION_NONE then
-		table.insert(vocationsIds, vocationId)
-	end
-
-	for _, member in ipairs(self:getMembers()) do
-		vocationId = member:getVocation():getBase():getId()
-		if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
-			table.insert(vocationsIds, vocationId)
-		end
-	end
-
-	local size = #vocationsIds
-	if size > 1 then
-		sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
-	end
-
-	return math.ceil((exp * sharedExperienceMultiplier) / (#self:getMembers() + 1))
+	return math.ceil(exp * (1 + (bonusExperiencePerPlayer / 100)) / totalMembers)
 end

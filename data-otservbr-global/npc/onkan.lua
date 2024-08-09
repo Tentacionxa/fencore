e6acef1e0827aa90cbe300af794c84aa0ab169c3
Tentacionxa <fencore05@gmail.com>
2024-08-09 22:44:41 +0100

    change

--- data-otservbr-global/npc/onkan.lua
+++ data-otservbr-global/npc/onkan.lua
@@ -120,6 +120,7 @@ npcConfig.shop = {
 	{ itemName = "might ring", clientId = 3048, buy = 5000 },
 	{ itemName = "enchanted theurgic amulet", clientId = 30403, buy = 2000000 },
 	{ itemName = "stone skin amulet", clientId = 3081, buy = 5000 },
+	{ itemName = "leviathan's amulet ", clientId = 9303, buy = 5000 },
 }
 -- On buy npc shop message
 npcType.onBuyItem = function(npc, player, itemId, subType, amount, ignore, inBackpacks, totalCost)

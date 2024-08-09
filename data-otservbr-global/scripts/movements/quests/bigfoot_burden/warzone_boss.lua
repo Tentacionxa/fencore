8cfd42015256a1429dc15a5e77baf585af7cb185
Tentacionxa <fencore05@gmail.com>
2024-08-09 22:55:28 +0100

    change

--- data-otservbr-global/scripts/movements/quests/bigfoot_burden/warzone_boss.lua
+++ data-otservbr-global/scripts/movements/quests/bigfoot_burden/warzone_boss.lua
@@ -34,16 +34,6 @@ function warzoneBoss.onStepIn(creature, item, pos, fromPosition)
 		return false
 	end
 
-	if warzone.locked then
-		creature:sendTextMessage(
-			MESSAGE_EVENT_ADVANCE,
-			"Please, wait until the room is cleared. \z
-		This happens 30 minutes after the last team entered."
-		)
-		creature:teleportTo(fromPosition)
-		return true
-	end
-
 	creature:teleportTo(warzone.teleportTo)
 	creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have half an hour to heroically defeat the \z
 	" .. warzone.boss .. ". Otherwise you'll be teleported out by the gnomish emergency device.")

aa02897ef558b9fe876f7c94b75bf7167a3d417d
Tentacionxa <fencore05@gmail.com>
2024-08-10 10:59:17 +0100

    change

--- data-otservbr-global/scripts/creaturescripts/quests/bigfoot_burden/versperoth_kill.lua
+++ data-otservbr-global/scripts/creaturescripts/quests/bigfoot_burden/versperoth_kill.lua
@@ -27,7 +27,7 @@ function versperothKill.onDeath(creature)
 
 	addEvent(transformTeleport, 1 * 60 * 1000, false)
 	addEvent(transformTeleport, 30 * 60 * 1000, true)
-	addEvent(warzoneConfig.spawnBoss, 1 * 80 * 1000, config.boss, config.bossResp)
+	addEvent(warzoneConfig.spawnBoss, 1 * 1 * 1000, config.boss, config.bossResp)
 	addEvent(warzoneConfig.resetRoom, 30 * 60 * 1000, config, "You were teleported out by the gnomish emergency device.", true)
 	return true
 end

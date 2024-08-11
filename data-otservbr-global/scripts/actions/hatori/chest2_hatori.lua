9f3e3952180050e6ebb4509feba0713543b927e3
Tentacionxa <fencore05@gmail.com>
2024-08-11 06:02:36 +0100

    change

--- data-otservbr-global/scripts/actions/hatori/chest2_hatori.lua
+++ data-otservbr-global/scripts/actions/hatori/chest2_hatori.lua
@@ -1,14 +1,14 @@
 local chest = Action()
 
 function chest.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if player:getStorageValue(37001) == 1 then
+	if player:getStorageValue(37543) == 1 then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The chest is empty.")
 		return true
 	end
 
 	player:addItem(14758, 1)
 	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You found an experience scroll.")
-	player:setStorageValue(37001, 1)
+	player:setStorageValue(37543, 1)
 	return true
 end
 

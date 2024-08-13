263231041343ac61cc8f763778673512e299f52e
Tentacionxa <fencore05@gmail.com>
2024-08-14 00:58:20 +0100

    change

--- data-canary/scripts/spells/attack/energy_wave.lua
+++ data-canary/scripts/spells/attack/energy_wave.lua
@@ -5,8 +5,8 @@ combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 combat:setArea(createCombatArea(AREA_SQUAREWAVE5, AREADIAGONAL_SQUAREWAVE5))
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 4.5)
-	local max = (level / 5) + (maglevel * 9)
+	local min = (level / 5) + (maglevel * 6.0)
+	local max = (level / 5) + (maglevel * 12)
 	return -min, -max
 end
 

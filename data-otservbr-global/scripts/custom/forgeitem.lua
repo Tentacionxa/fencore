ad9541783a8ad8eb94faac6c12d768fc2e23f310
Tentacionxa <fencore05@gmail.com>
2024-08-20 09:04:20 +0100

    change

--- data-otservbr-global/scripts/custom/forgeitem.lua
+++ data-otservbr-global/scripts/custom/forgeitem.lua
@@ -11,7 +11,7 @@ end
 Karin.Forge = {
     Config = {
         tokenId = 943,  -- tutaj daj id tokena
-        Default = 53, -- tutaj ustawiasz szanse na tier
+        Default = 60, -- tutaj ustawiasz szanse na tier
         Tier = {
             [1] = {
                 [1] = 25000,

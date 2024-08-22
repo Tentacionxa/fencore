984471f02ad8737dfa150dce520e5e850542d9f0
Leandro <leandro.rufma@gmail.com>
2024-08-22 13:51:44 -0300

    change starttime to 18h

--- data-otservbr-global/scripts/custom/happy_hour_experience_bonus.lua
+++ data-otservbr-global/scripts/custom/happy_hour_experience_bonus.lua
@@ -1,5 +1,5 @@
-local lootWars = GlobalEvent("happyHourExperienceBonus")
-function lootWars.onTime(interval)
+local happyHourEvent = GlobalEvent("happyHourExperienceBonus")
+function happyHourEvent.onTime(interval)
     local weekDay = os.date("*t").wday
     if weekDay == 1 or weekDay == 7 then
         return
@@ -9,5 +9,5 @@ function lootWars.onTime(interval)
         targetPlayer:sendTextMessage(MESSAGE_ADMINISTRATOR, "THE HAPPY HOUR DOUBLE XP EVENT IS ON! GRAB YOUR WEAPONS AND BE READY TO HUNT!")
 	end
 end
-lootWars:time("20:00:00")
-lootWars:register()
+happyHourEvent:time("18:00:00")
+happyHourEvent:register()

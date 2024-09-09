433dfa66fb7506e632662e9b2ac5d8dc5af14a7b
Tentacionxa <fencore05@gmail.com>
2024-09-09 19:53:23 +0100

    change

--- data/scripts/spells/attack/strong_ethereal_spear.lua
+++ data/scripts/spells/attack/strong_ethereal_spear.lua
@@ -6,7 +6,7 @@ combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
 
 function onGetFormulaValues(player, skill, attack, factor)
 	local levelTotal = player:getLevel() / 5
-	return -(((2 * skill + attack / 2500) * 1.30) + levelTotal + 7), -(((2 * skill + attack / 1875) * 7.50) + levelTotal + 13)
+	return -(((2 * skill + attack / 2500) * 3.50) + levelTotal + 7), -(((2 * skill + attack / 1875) * 7.50) + levelTotal + 13)
 end
 
 combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")

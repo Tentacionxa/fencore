ec89e56f0adb68e9803b2ce5dfa38794dc174d24
Tentacionxa <fencore05@gmail.com>
2024-09-09 22:39:30 +0100

    change

--- src/creatures/players/wheel/wheel_gems.cpp
+++ src/creatures/players/wheel/wheel_gems.cpp
@@ -416,11 +416,11 @@ void WheelModifierContext::addStrategies(WheelGemSupremeModifier_t modifier) {
 			m_strategies.push_back(std::make_unique<GemModifierSpellBonusStrategy>(m_wheel, "Great Death Beam", bonus));
 			break;
 		case WheelGemSupremeModifier_t::Sorcerer_HellsCore_DamageIncrease:
-			bonus.increase.damage = 18;
+			bonus.increase.damage = 16;
 			m_strategies.push_back(std::make_unique<GemModifierSpellBonusStrategy>(m_wheel, "Hell's Core", bonus));
 			break;
 		case WheelGemSupremeModifier_t::Sorcerer_HellsCore_CriticalExtraDamage:
-			bonus.increase.criticalDamage = 11;
+			bonus.increase.criticalDamage = 10;
 			m_strategies.push_back(std::make_unique<GemModifierSpellBonusStrategy>(m_wheel, "Hell's Core", bonus));
 			break;
 		case WheelGemSupremeModifier_t::Sorcerer_EnergyWave_DamageIncrease:

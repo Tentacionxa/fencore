108f94603e980ea598c38fa7ac6c22338373f061
Tentacionxa <fencore05@gmail.com>
2024-09-09 18:48:24 +0100

    fixed sleepshawl scripts

--- data/scripts/talkactions/player/repair_sleepshawl.lua
+++ data/scripts/talkactions/player/repair_sleepshawl.lua
@@ -1,7 +1,7 @@
 local sleepRepair = TalkAction("!sleepshawl")
 local config = {
-	wormSoftId = 22516,
-	newSoftId = 30342,
+	wormSoftId = 29428,
+	newSoftId = 29428,
 	value = 75000,
 }
 
@@ -11,14 +11,14 @@ function sleepRepair.onSay(player, words, param)
 			player:removeItem(config.wormSoftId, 1)
 			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
 			player:addItemStoreInboxEx(Game.createItem(config.newSoftId, 1), 1, self)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You charged a soft boots successfully.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You charged Your Sleep Shawl successfully.")
 		else
 			player:getPosition():sendMagicEffect(CONST_ME_POFF)
 			player:sendCancelMessage("You do not have enough money!")
 		end
 	else
 		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:sendCancelMessage("You do not have worn soft boots!")
+		player:sendCancelMessage("You do not have a Sleep Shawl!")
 	end
 	return false
 end

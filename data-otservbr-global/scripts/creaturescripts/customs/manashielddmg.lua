d784cbea34a949b6c6bb203e9f0907b2146c2e7a
Tentacionxa <fencore05@gmail.com>
2024-09-11 18:06:25 +0100

    change

--- data-otservbr-global/scripts/creaturescripts/customs/manashielddmg.lua
+++ data-otservbr-global/scripts/creaturescripts/customs/manashielddmg.lua
@@ -1,22 +1,21 @@
-
 local manaDamage = CreatureEvent("IncreaseManaDamageEvent")
 
-function manaDamage.onManaChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType, ori>
-        if not attacker then
-                return primaryDamage, primaryType, secondaryDamage, secondaryType
-        end
+function manaDamage.onManaChange(creature, attacker, primaryDamage, primaryType, secondaryDamage, secondaryType, origin)
+	if not attacker then
+		return primaryDamage, primaryType, secondaryDamage, secondaryType
+	end
+	
+	if primaryType == COMBAT_HEALING or secondaryType == COMBAT_HEALING then
+		return primaryDamage, primaryType, secondaryDamage, secondaryType
+	end
+	
+	local amount = 2.2 --Increase Dmg 200% [[1.1 = +10% dmg, 1.5 = +50% dmg]]
+	
+	if amount > 0 then
+		return primaryDamage * amount, primaryType, amount * secondaryDamage, secondaryType
+	end
 
-        if primaryType == COMBAT_HEALING or secondaryType == COMBAT_HEALING then
-                return primaryDamage, primaryType, secondaryDamage, secondaryType
-        end
-
-        local amount = 2.2 --Increase Dmg 200% [[1.1 = +10% dmg, 1.5 = +50% dmg]]
-
-        if amount > 0 then
-                return primaryDamage * amount, primaryType, amount * secondaryDamage, secondaryType
-        end
-
-        return primaryDamage, primaryType, secondaryDamage, secondaryType
+	return primaryDamage, primaryType, secondaryDamage, secondaryType
 end
 
 manaDamage:register()

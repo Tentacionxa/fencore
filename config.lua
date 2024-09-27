8922b0233bfb5db1f7eeab363626f2e14dadf2ca
Tentacionxa <fencore05@gmail.com>
2024-09-27 17:19:36 +0100

    change

--- config.lua
+++ config.lua
@@ -144,7 +144,7 @@ forgeMaxSlivers = 7
 forgeInfluencedLimit = 300
 forgeFiendishLimit = 300
 forgeFiendishIntervalType = "minute"
-forgeFiendishIntervalTime = "60"
+forgeFiendishIntervalTime = "1"
 
 ruseChanceFormulaA = 0.0307576
 ruseChanceFormulaB = 0.440697

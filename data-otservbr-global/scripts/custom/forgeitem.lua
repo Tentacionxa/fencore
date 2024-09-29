c50ede83c3146ffa1c0e073d969bcf27b96ca4c4
Tentacionxa <fencore05@gmail.com>
2024-09-29 14:56:13 +0100

    holy soil tier chance down to 30

--- data-otservbr-global/scripts/custom/forgeitem.lua
+++ data-otservbr-global/scripts/custom/forgeitem.lua
@@ -11,7 +11,7 @@ end
 Karin.Forge = {
     Config = {
         tokenId = 943,  -- tutaj daj id tokena
-        Default = 40, -- tutaj ustawiasz szanse na tier
+        Default = 30, -- tutaj ustawiasz szanse na tier
         Tier = {
             [1] = {
                 [1] = 25000,

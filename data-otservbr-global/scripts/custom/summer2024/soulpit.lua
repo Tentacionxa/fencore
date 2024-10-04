73c75ff62f999ac23feac52a689814fad5a5fe92
Tentacionxa <fencore05@gmail.com>
2024-10-04 08:47:11 +0100

    fix

--- data-otservbr-global/scripts/custom/summer2024/soulpit.lua
+++ data-otservbr-global/scripts/custom/summer2024/soulpit.lua
@@ -14,7 +14,7 @@ SoulPit.config = {
 	rangeMonsters = 6,
     timeInMinutes = 10,
     checkMonstersInterval = 5 * 1000,
-    soulPitItemId = 47360,
+    soulPitItemId = 47367,
     firstSoulCoreItemId = 47381,
     lastSoulCoreItemId = 49163,
 }

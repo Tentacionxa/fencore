a85780239bab5143e8b64dfb691e31a3593e3a4c
Tentacionxa <fencore05@gmail.com>
2024-10-10 05:47:59 +0100

    removed old xp boost

--- data/modules/scripts/gamestore/gamestore.lua
+++ data/modules/scripts/gamestore/gamestore.lua
@@ -7527,14 +7527,6 @@ GameStore.Categories = {
 		state = GameStore.States.STATE_NONE,
 		offers = {
 			{
-				icons = { "XP_Boost.png" },
-				name = "XP Boost",
-				price = 100,
-				id = 65010,
-				description = "<i>Purchase a boost that increases the experience points your character gains from hunting by 50%!</i>\n\n{character}\n{info} lasts for 1 hour hunting time\n{info} paused if stamina falls under 14 hours\n{info} can be purchased up to 9 times between 2 server saves\n{info} price increases with every purchase\n{info} cannot be purchased if an XP boost is already active",
-				type = GameStore.OfferTypes.OFFER_TYPE_EXPBOOST,
-			},
-			{
 				icons = { "XP_Boost_Scroll.png" },
 				name = "Gold Experience Scroll",
 				price = 1300,

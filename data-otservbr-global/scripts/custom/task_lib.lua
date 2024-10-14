a6a601eae15f5fd399bc45cdfb1675365b1ff3eb
Tentacionxa <fencore05@gmail.com>
2024-10-14 17:35:05 +0100

    added reward to griish task

--- data-otservbr-global/scripts/custom/task_lib.lua
+++ data-otservbr-global/scripts/custom/task_lib.lua
@@ -750,8 +750,10 @@ taskConfiguration = {
 
 {name = "Griish - Griish Knight, Griish Lord, Griish Scout, Griish Leader, Griish Sorcereress, Griish Mage, Griish Druid, Griish Priestess, Griish Warior, Griish Wizard, Griish Adventurer, Griish Hunter", color = 40, total = 3000, type = "repeatable", storage = 190945, storagecount = 190946,
         rewards = {
-			{46316, 35},
-	{"exp", 9000000},
+			{46316, 200},
+			{46314, 200},
+			{37317, 5},
+	{"exp", 20000000},
         },
         races = {
                 "Griish Knight",

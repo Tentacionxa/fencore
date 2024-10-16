2ef821a0c0e3a7a38096cc053beeb1c99741f567
Tentacionxa <fencore05@gmail.com>
2024-10-16 16:00:33 +0100

    fix dusts add max 100  on 1 use

--- data-otservbr-global/scripts/forgedust.lua
+++ data-otservbr-global/scripts/forgedust.lua
@@ -1,20 +1,28 @@
 local dust = Action()
 
 function dust.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-local amount = 1 -- quantidade de dusts que o item vai dar
-local totalDusts = player:getForgeDusts()
-local limitDusts = 100 -- quantidade maxima de dusts
-  
-	if (totalDusts + amount) < limitDusts then
-		player:addForgeDusts(amount)
-	  
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You gained "..amount.." dusts")
-		item:remove(1)
-		return true
-	else
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "It is not possible to add over 100 Forge Dusts")
-	end
+    local itemId = 49181 -- ID of the item to be transformed into dusts
+    local amountPerItem = 1 -- Amount of dusts each item provides
+    local totalDusts = player:getForgeDusts()
+    local limitDusts = 100 -- Maximum amount of dusts allowed
+    local itemCount = player:getItemCount(itemId) -- Count of item ID 49181 the player has
+
+    -- Calculate how many dusts can be added without exceeding the limit
+    local availableDusts = limitDusts - totalDusts
+    local dustToAdd = math.min(itemCount, availableDusts)
+
+    if dustToAdd > 0 then
+        player:addForgeDusts(dustToAdd)
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You gained " .. dustToAdd .. " dusts.")
+        
+        -- Remove the corresponding number of items
+        item:remove(dustToAdd)
+        
+        return true
+    else
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You cannot add more Forge Dusts. Limit reached.")
+    end
 end
 
-dust:id(49181) -- ID do item a ser transformado em dusts
+dust:id(49181) -- ID of the item to be transformed into dusts
 dust:register()

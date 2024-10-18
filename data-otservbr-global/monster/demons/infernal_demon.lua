ce9b1592a932668aa72fd7d691bc5fa2963cf6e3
Tentacionxa <fencore05@gmail.com>
2024-10-18 22:07:20 +0100

    change exp

--- data-otservbr-global/monster/demons/infernal_demon.lua
+++ data-otservbr-global/monster/demons/infernal_demon.lua
@@ -2,7 +2,7 @@ local mType = Game.createMonsterType("Infernal Demon")
 local monster = {}
 
 monster.description = "an infernal demon"
-monster.experience = 160000
+monster.experience = 180000
 monster.outfit = {
 	lookType = 1313,
 	lookHead = 0,

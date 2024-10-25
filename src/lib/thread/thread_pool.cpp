465a46444bb1e816032a069a2a6f2af6fd4956ef
Tentacionxa <fencore05@gmail.com>
2024-10-25 17:10:29 +0100

    fix

--- src/lib/thread/thread_pool.cpp
+++ src/lib/thread/thread_pool.cpp
@@ -8,37 +8,38 @@
  */
 
 #include "pch.hpp"
-
 #include "lib/thread/thread_pool.hpp"
 #include "lib/logging/logger.hpp"
 #include "game/game.hpp"
 #include "utils/tools.hpp"
-ThreadPool threadPool(logger);
-extern Logger logger;
-Logger globalLogger;  // Create an instance of Logger (adjust this as per your Logger setup)
-ThreadPool threadPool(globalLogger);  // Initialize the global ThreadPool instance with Lo
-/**
- * Regardless of how many cores your computer have, we want at least
- * 4 threads because, even though they won't improve processing they
- * will make processing non-blocking in some way and that would allow
- * single core computers to process things concurrently, but not in parallel.
- */
 
+// Global Logger instance for the application
+Logger globalLogger;
+
+// Global ThreadPool instance using the global logger
+ThreadPool threadPool(globalLogger);
+
+/**
+ * Regardless of how many cores your computer has, we want at least
+ * 4 threads because, even though they won't improve processing, they
+ * will make processing non-blocking, allowing single-core computers
+ * to process things concurrently, albeit not in parallel.
+ */
 #ifndef DEFAULT_NUMBER_OF_THREADS
-	#define DEFAULT_NUMBER_OF_THREADS 4
+    #define DEFAULT_NUMBER_OF_THREADS 4
 #endif
 
 ThreadPool::ThreadPool(Logger &logger) :
-	BS::thread_pool(std::max<int>(getNumberOfCores(), DEFAULT_NUMBER_OF_THREADS)), logger(logger) {
-	start();
+    BS::thread_pool(std::max<int>(getNumberOfCores(), DEFAULT_NUMBER_OF_THREADS)), logger(logger) {
+    start();
 }
 
 void ThreadPool::start() {
-	logger.info("Running with {} threads.", get_thread_count());
+    logger.info("Running with {} threads.", get_thread_count());
 }
 
 void ThreadPool::shutdown() {
-	logger.info("Shutting down thread pool...");
-	stopped = true;
-	wait();
+    logger.info("Shutting down thread pool...");
+    stopped = true;
+    wait();
 }
